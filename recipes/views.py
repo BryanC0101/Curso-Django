1452d91b3b91ce78226d8b36fe65542886bccfb1
Bryan Cauê Vaz <bcaue177@gmail.com>
2026-03-19 21:28:28 -0300

    Renovido 'contato' e 'sobre'

--- recipes/views.py
+++ recipes/views.py
@@ -1,16 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 def home(request):
     return render(request, 'recipes/home.html', context={
         'name': 'Bryan'
     })
-
-
-def sobre(request):
-    return HttpResponse('SOBRE')
-
-
-def contato(request):
-    return HttpResponse('CONTATO')
