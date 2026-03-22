4065dcc20b43acddb0ea7ede0b3b1d7926f12e9a
Bryan Cauê Vaz <bcaue177@gmail.com>
2026-03-22 18:50:12 -0300

    title changed

--- projeto/settings.py
+++ projeto/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'projeto.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'base-templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
